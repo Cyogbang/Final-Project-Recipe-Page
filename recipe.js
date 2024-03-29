ccf400fe55d92b565a05c8aeeb29768d8d68cd6f
Cy <ogbangcyprian360@gmail.com>
2024-03-29 23:11:45 +0100

    Reg point fixed. JS advanced

--- recipe.js
+++ recipe.js
@@ -1,16 +1,20 @@
+const clientNAME = document.getElementById("name");
+const clientPHONE = document.getElementById("phone");
+const clientMAIL = document.getElementById("mail");
+const errorMSG = document.querySelector(".error");
+const successMSG = document.getElementById("success");
+const cautionMSG = document.getElementById("caution");
+
 function reg () {
-    const clientNAME = document.getElementById("name");
-    const clientPHONE = document.getElementById("phone");
-    const clientMAIL = document.getElementById("mail");
-    const errorMSG = document.querySelector(".error");
-    const successMSG = document.getElementById("success");
-    const cautionMSG = document.getElementById("caution");
 
-    const inputNAME = clientNAME.value;
-    const inputPHONE = clientPHONE.value;
-    const inputMAIL = clientMAIL.value;
+    console.log(clientNAME.value);
+    console.log(clientMAIL.value);
+    console.log(errorMSG.value);
+    console.log(successMSG.value);
+    console.log(clientPHONE.value);
+    console.log("click");
 
-    if (inputNAME || inputPHONE || inputMAIL === "") {
+    if (clientNAME.value == "" || clientPHONE.value == "" || clientMAIL.value == "") {
         console.log("enter a value")
         errorMSG.style.display = "block";
     } else {
@@ -18,54 +22,122 @@ function reg () {
         // errorMSG.style.display = "none"
         successMSG.style.display = "block";
         cautionMSG.style.display = "none";
+
+        errorMSG.style.display = "none"
     }
 }
 
 function search () {
     const SearchERROR = document.getElementById("SRerror");
     const searcher = document.getElementById("searching");
-    const Afang = document.getElementById("Dish1");
-    const CoconutRICE = document.getElementById("Dish2");
-    const WhiteSOUP = document.getElementById("Dish3");
-    const ChickenSTEW = document.getElementById("Dish4");
-    const Coleslaw = document.getElementById("Dish5");
+    const Afang = document.getElementById("afang_dish");
+    const CoconutRICE = document.getElementById("coconutRice_dish");
+    const WhiteSOUP = document.getElementById("whiteSoup_dish");
+    const ChickenSTEW = document.getElementById("chickenStew_dish");
+    const Coleslaw = document.getElementById("coleslaw_dish");
+
+    // console.log(SearchERROR.value)
+    // // console.log(searcher.value)
+    // console.log(Afang.value)
+    // console.log(CoconutRICE.value)
+    // console.log(WhiteSOUP.value)
+    // console.log(ChickenSTEW.value)
+    // console.log(Coleslaw.value)
+
+    if (searcher.value == "") {
+        console.log("empty");
+        SearchERROR.style.display = "block"
+    } else if (searcher.value == "Coconut Rice") {
+        console.log("coconut Rice")
+        SearchERROR.style.display = "none";
+        CoconutRICE.style.display = "block";
+        ChickenSTEW.style.display = "none";
+        WhiteSOUP.style.display = "none";
+        Coleslaw.style.display = "none";
+        Afang.style.display = "none";                  
+    } else if (searcher.value == "Chicken Stew" || "chicken stew" || "Chicken stew") {
+        console.log("stew");
+        SearchERROR.style.display = "none";
+        ChickenSTEW.style.display = "block";
+        WhiteSOUP.style.display = "none";
+        Coleslaw.style.display = "none";
+        Afang.style.display = "none";
+        CoconutRICE.style.display = "none"; 
+    } else if (searcher.value == "White soup" || "White Soup" || "white soup") {
+        console.log("White Soup");
+        SearchERROR.style.display = "none";
+        WhiteSOUP.style.display = "block";
+        Coleslaw.style.display = "none";
+        Afang.style.display = "none";
+        CoconutRICE.style.display = "none";
+        ChickenSTEW.style.display = "none";
+    } else if (searcher.value == "Coleslaw Salad") {
+        console.log("salad");
+        SearchERROR.style.display = "none";
+        Coleslaw.style.display = "block";
+        Afang.style.display = "none";
+        CoconutRICE.style.display = "none";
+        ChickenSTEW.style.display = "none"
+        WhiteSOUP.style.display = "none";
+    } else if (searcher.value == "Afang" || "Afang Soup") {
+        console.log("Soup");
+        SearchERROR.style.display = "none";
+        Afang.style.display = "block";
+        CoconutRICE.style.display = "none";
+        ChickenSTEW.style.display = "none"
+        WhiteSOUP.style.display = "none";
+        Coleslaw.style.display = "none";
+    } else {
+        SearchERROR.style.display = "block";
+    }
+}
+    
     // const recipe = document.querySelector(".recipes");
 
-    const SR = searcher.value;
-    const Dishes = [Afang, CoconutRICE, WhiteSOUP, ChickenSTEW, Coleslaw];
+    // const SR = searcher.value;
+    // const Dishes = [Afang, CoconutRICE, WhiteSOUP, ChickenSTEW, Coleslaw];
 
-    // for (let x = 0; x <= recipe.length; x++) {
-    //     console.log(x);
-    //     const element = array[x];
+    // // for (let x = 0; x <= recipe.length; x++) {
+    // //     console.log(x);
+    // //     const element = array[x];
         
-        if (SR == "") {
-            SearchERROR.style.display = "block";
-        } else if (SR == "Afang" || "Afang Soup") {
-            console.log(Dishes[0])
-            Dishes[0]
-        } else if (SR == "Coconut Rice") {
-            console.log(Dishes[1])
-            Dishes[1]
-        } else if (SR == "White Soup") {
-            console.log(Dishes[2])
-            Dishes[2]
-        } else if (SR == "Chicken Stew") {
-            console.log(Dishes[3])
-            Dishes[3]
-        } else if (SR == "Coleslaw Salad" || "Coleslaw" || "Salad") {
-            console.log(Dishes[4])
-            Dishes[4]
-        } else {
-            SearchERROR.style.display = "block";
-        }
-    }
-// }
+    //     if (SR == "") {
+    //         SearchERROR.style.display = "block";
+    //     } else if (SR == "Afang" || "Afang Soup") {
+    //         console.log(Dishes[0])
+    //         Dishes[0]
+    //     } else if (SR == "Coconut Rice") {
+    //         console.log(Dishes[1])
+    //         Dishes[1]
+    //     } else if (SR == "White Soup") {
+    //         console.log(Dishes[2])
+    //         Dishes[2]
+    //     } else if (SR == "Chicken Stew") {
+    //         console.log(Dishes[3])
+    //         Dishes[3]
+    //     } else if (SR == "Coleslaw Salad" || "Coleslaw" || "Salad") {
+    //         console.log(Dishes[4])
+    //         Dishes[4]
+    //     } else {
+    //         SearchERROR.style.display = "block";
+    //     }
+    // }
+
 
 function aDish () {
-    let afang = document.getElementById("afang_dish");
+    const afang = document.getElementById("afang_dish");
 
     afang.style.display = "block";
 }
+
+// console.log(afang)
+// console.log(afang.style.display)
+
+// if (afang.style.display === 'block') {
+//     console.log("no image");
+// } else {
+//     console.log("dish");
+// }
 
 function bDish () {
     let coconutRice = document.getElementById("coconutRice_dish");
